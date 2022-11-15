a1d3cf6c6cc67f91f0caaca83d0f2870a169afff
Maximilian <scientificresearch309@gmail.com>
2022-11-15 21:40:47 +0100

    if anf if -else

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.scss";
 
+const title = "Our JSX Basics Lesson";
+const userIsOnline = true;
+
 function App() {
   return (
     <div className="App">
-      <h1>Test App</h1>
+      <h1>{title}</h1>
+
+      {/* strange if statement */}
+      {userIsOnline && <div>user is online</div>}
       <p>This is a Test App with Vite.</p>
+      <hr />
+
+      {/* strange Eternary Operator */}
+
+      {userIsOnline ? (
+        <div>user doesn't need to login!</div>
+      ) : (
+        <div>User needs to login!</div>
+      )}
     </div>
   );
 }
